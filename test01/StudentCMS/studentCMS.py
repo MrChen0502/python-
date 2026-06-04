a460566519753c3219c058a18d1b101ba58cce5c
霓壹 <chenganghao9@gmail.com>
2026-06-04 14:50:04 +0800

    python基础学习——python实现学生管理系统 面向对象

--- test01/StudentCMS/studentCMS.py
+++ test01/StudentCMS/studentCMS.py
@@ -29,7 +29,19 @@ class StudentCMS(object):
 
     # 4.定义函数，实现添加学生信息功能
     def add_student(self):
-        pass
+        # 4.1 提示用户输入学生信息 并接收
+        name = input("请输入学生姓名：")
+        gender = input("请输入学生性别：")
+        age = int(input("请输入学生年龄："))  # 可能是整数，需要转换类型
+        phone = input("请输入学生手机号：")
+        desc = input("请输入学生描述信息：")
+        # 4.2 把上述信息封装成学生对象
+        stu = Student(name, gender, age, phone, desc)
+        # 4.2 把学生对象添加到列表中
+        self.stu_list.append(stu)
+        # 4.4 提示
+        print(f"添加{name}学生信息成功！！ \n")
+
 
     # 5.删除学员
     def del_student(self):
@@ -45,7 +57,13 @@ class StudentCMS(object):
 
     # 8.查询所有
     def search__all_student(self):
-        pass
+        # 8.1 判断列表长度是否为0 如果为0 提示：暂无学生信息 请添加后查询
+        if len(self.stu_list) == 0:
+            print("暂无学生信息 请添加后查询！\n")
+        else:
+            # 8.2 如果长度不为0 遍历列表 打印出所有的学生信息
+            for i in self.stu_list:
+                print(f"学生姓名:{i.name},学生性别:{i.gender},学生年龄:{i.age},学生手机号:{i.phone},学生简介信息:{i.desc}\n")
 
     # 9.保存学生信息
     def save_student(self):
@@ -68,7 +86,7 @@ class StudentCMS(object):
             # 11.6 根据用户输入的编号 做不同的操作
             if input_num == "1":
                 # 添加学生信息
-                print("添加学生信息 \n")
+                # print("添加学生信息 \n")
                 self.add_student()
             elif input_num == "2":
                 # 删除学生信息
