2d1de916f727394fafdf2cd0659a6cfca4284f7e
霓壹 <chenganghao9@gmail.com>
2026-06-06 16:20:06 +0800

    python基础学习——网络编程

---
+++ test01/day04/02_网编案例_一句胡_服务器端.py
@@ -0,0 +1,33 @@
+"""
+案例：网编入门案例 服务器端给客户发送消息 客户端给出回执信息
+
+服务器端开发流程：
+    1.创建服务器端Socket对象
+    2.绑定IP地址和端口号
+    3.设置最大监听数
+    4.等待客户端申请建立连接
+    5.给客户端发送消息
+    6.接收客户端的信息并打印
+    7.释放资源
+"""
+
+# 导包
+import socket
+
+
+# 1.创建服务器端Socket对象
+server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+# 2.绑定IP地址和端口号
+server_socket.bind(('127.0.0.1' , 10000))
+# 3.设置最大监听数
+server_socket.listen(5)
+# 4.等待客户端申请建立连接
+accept_socket , client_info = server_socket.accept()
+# 5.给客户端发送消息
+accept_socket.send(b'Welcome To Socket!')
+# 6.接收客户端的信息并打印
+data = accept_socket.recv(1024).decode('utf-8')
+print(f"收到客户端消息：{data}")
+print(f"客户端地址：{client_info}")
+# 7.释放资源
+accept_socket.close()
