892a6fbd8452adcdf4737ee8cb9c7acfb423c3ba
霓壹 <chenganghao9@gmail.com>
2026-06-06 15:32:20 +0800

    python基础学习——深浅拷贝

--- test01/day03/13_深浅拷贝_面试题.py
+++ test01/day03/13_深浅拷贝_面试题.py
@@ -31,5 +31,79 @@ def dm01_普通赋值():
     print('id(c)→', id(c))  #
     print('id(d)→', id(d))  #
 
+
+
+import copy
+
+def dm02_浅拷贝可变类型():
+    a = [1, 2, 3]
+    b = [11, 22, 33]
+    c = [6, 7, a, b]
+
+    d = copy.copy(c)
+
+    print('id(c)→', id(c))
+    print('id(d)→', id(d))
+
+    # 测试2
+    print(id(c[2]))
+    print(id(a))
+
+    # 修改a[2] = 22
+    a[2] = 22
+    # c[0] = 100
+    print('c→', c)
+    print('d→', d)
+
+import copy
+
+def dm03_浅拷贝不可变类型():
+    # 不可变类型 a b c
+    a = (1, 2, 3)        # 0x01
+    b = (11, 22, 33)     # 0x02
+    c = (6, 7, a, b)     # 0x03
+
+    d = copy.copy(c)
+
+    print('id(c)→', id(c))  # 0x03
+    print('id(d)→', id(d))  # 0x03
+
+
+import copy
+
+
+def dm04_深拷贝可变类型():
+    a = [1, 2, 3]
+    b = [11, 22, 33]
+    c = [6, 7, a, b]
+
+    d = copy.deepcopy(c)
+
+    print('id(c)→', id(c))  # 比如 0x100
+    print('id(d)→', id(d))  # 比如 0x200 (不同)
+
+    a[1] = 100
+    b[1] = 800
+
+    print(f'c: {c}')  # c: [6, 7, [1, 100, 3], [11, 800, 33]]
+    print(f'd: {d}')  # d: [6, 7, [1, 2, 3], [11, 22, 33]] (没变！)
+
+    import copy
+
+def dm05_深拷贝不可变类型():
+    a = (1, 2, 3)
+    b = (11, 22, 33)
+    c = (a, b)
+
+    d = copy.deepcopy(c)
+    print(id(c))  #
+    print(id(d))  #
+
+
+
 if __name__ == '__main__':
-    dm01_普通赋值()
+    # dm01_普通赋值()
+    # dm02_浅拷贝可变类型()
+    # dm03_浅拷贝不可变类型()
+    # dm04_深拷贝可变类型()
+    dm03_浅拷贝不可变类型()
