48e203b859565a04ea00919da5841406a809700e
霓壹 <chenganghao9@gmail.com>
2026-06-13 14:07:20 +0800

    python基础学习——网络机器人入门(爬虫)

--- test01/第四章/01.网络机器人入门程序.py
+++ test01/第四章/01.网络机器人入门程序.py
@@ -1,4 +1,5 @@
 import requests
+from lxml import html
 
 # 定义url
 targe_url = "https://www.tiobe.com/tiobe-index/"
@@ -7,4 +8,16 @@ targe_url = "https://www.tiobe.com/tiobe-index/"
 response =  requests.get(targe_url)
 
 # 输出数据到控制台
-print(response.text)
+# print(response.text)
+document = html.fromstring(response.text)
+
+# 解析数据
+# 解析表头
+th_list = document.xpath("//table[@id='top20']/thead/tr/th/text()")
+print(th_list)
+
+# 解析表格数据
+td_list = document.xpath("//tbody/tr")
+for td in td_list:
+    tds_list = td.xpath("./td/text()")
+    print(tds_list)
