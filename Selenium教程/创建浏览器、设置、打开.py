d402c66c44785ff39032b67b1a3e7f03c9a9e8c0
霓壹 <chenganghao9@gmail.com>
2026-06-18 14:10:36 +0800

    selenium学习

---
+++ Selenium教程/创建浏览器、设置、打开.py
@@ -0,0 +1,34 @@
+# 1. 禁用沙盒（一般不需要，除非在服务器跑）
+# options.add_argument('--no-sandbox')
+import time
+
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options          # 用于设置 Edge 配置
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+# 设置浏览器、启动浏览器
+def she(url):
+    # 创建设置浏览器对象
+    q1 = Options()
+
+    # 2. 保持浏览器打开（调试时很有用）
+    q1.add_experimental_option('detach', True)
+
+    # 3. 启动浏览器（用 Edge）
+    driver = webdriver.Edge(options=q1)
+    driver.get(url)
+    # driver.get("https://www.douban.com") ==> driver = webdriver.Edge(service=Service('msedgedriver.exe'), options=q1)
+    return driver
+
+a1 = she("https://www.baidu.com")
+print(a1.title)
+# a1.get("https://www.baidu.com")
+
+# 关闭标签页
+time.sleep(2)
+a1.close()
+
+# 退出浏览器(关闭所有标签页)
+a1.quit()
+
